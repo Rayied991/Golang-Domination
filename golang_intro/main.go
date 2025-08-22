364e377faffb8c6618ce556dcd7f4375a37e12a7
RRayied991 <hussainrayied9@gmail.com>
2025-08-22 21:08:00 +0600

    Golang intro added

---
+++ golang_intro/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"myLearning/myutil"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+	myutil.PrintMessage("Hello Golang")
+}
---
+++ golang_intro/myutil/myutil.go
@@ -0,0 +1,7 @@
+// packages
+package myutil
+
+import "fmt"
+func PrintMessage(message string){
+	fmt.Println(message);
+}
